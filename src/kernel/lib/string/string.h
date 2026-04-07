3124e6aa8a35be8640dfeb1d4cd906a43dad8eec
Payton Chenault <pchenault08@gmail.com>
2026-04-07 00:52:55 -0700

    added memset + cpy

--- src/kernel/lib/string/string.h
+++ src/kernel/lib/string/string.h
@@ -8,4 +8,7 @@ int strcmp(const char* s1, const char* s2);
 char* strtok(char* str, const char* delimiters);
 char* strcpy(char* dest, const char* src);
 
+void* memset(void* ptr, int value, size_t num);
+void* memcpy(void* dest, const void* src, size_t num);
+
 #endif
